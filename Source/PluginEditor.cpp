c8cfb1935be420a15b558ebba2f4a49cf3e1b526
lmaser <luismartinserradilla@gmail.com>
2026-05-30 01:46:43 +0200

    rev71e

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -2516,6 +2516,7 @@ void FREQTRAudioProcessorEditor::openNumericEntryPopupForSlider (juce::Slider& s
     lnf.setScheme (activeScheme);
     const auto scheme = activeScheme;
 
+    juce::String prefix;
     juce::String suffix;
     juce::String suffixShort;
     const bool isFreqSyncMode = (&s == &freqSlider && syncButton.getToggleState());
@@ -2532,7 +2533,7 @@ void FREQTRAudioProcessorEditor::openNumericEntryPopupForSlider (juce::Slider& s
             suffixShort = " Hz";
         }
     }
-    else if (&s == &modSlider)       { suffix = " X MOD";      suffixShort = " X MOD"; }
+    else if (&s == &modSlider)       { prefix = "X";           suffix = " MOD";      suffixShort = " MOD"; }
     else if (&s == &feedbackSlider)  { suffix = " % FBK";      suffixShort = " % FBK"; }
     else if (&s == &jitterSlider)    { suffix = " % JIT";      suffixShort = " % JIT"; }
     else if (&s == &combSlider)      { suffix = " Hz";         suffixShort = " Hz"; }
@@ -2545,6 +2546,7 @@ void FREQTRAudioProcessorEditor::openNumericEntryPopupForSlider (juce::Slider& s
     else if (&s == &outputSlider)    { suffix = " dB OUTPUT";  suffixShort = " dB OUT"; }
     else if (&s == &tiltSlider)      { suffix = " dB TILT";    suffixShort = " dB TILT"; }
     else if (&s == &limThresholdSlider) { suffix = " dB LIM";  suffixShort = " dB LIM"; }
+    const juce::String prefixText = prefix.trim();
     const juce::String suffixText = suffix.trimStart();
     const juce::String suffixTextShort = suffixShort.trimStart();
     const bool isPercentPrompt = (&s == &engineSlider || &s == &harmSlider || &s == &mixSlider || &s == &feedbackSlider || &s == &jitterSlider || &s == &panSlider);
@@ -2570,6 +2572,7 @@ void FREQTRAudioProcessorEditor::openNumericEntryPopupForSlider (juce::Slider& s
 
     aw->addTextEditor ("val", currentDisplay, juce::String());
 
+    juce::Label* prefixLabel = nullptr;
     juce::Label* suffixLabel = nullptr;
 
     struct SyncDivisionInputFilter : juce::TextEditor::InputFilter
@@ -2606,6 +2609,13 @@ void FREQTRAudioProcessorEditor::openNumericEntryPopupForSlider (juce::Slider& s
         r.setHeight ((int) (f.getHeight() * kPromptEditorHeightScale) + kPromptEditorHeightPadPx);
         r.setY (juce::jmax (kPromptEditorMinTopPx, r.getY() - kPromptEditorRaiseYPx));
         editorBaseBounds = r;
+
+        prefixLabel = new juce::Label ("prefix", prefixText);
+        prefixLabel->setJustificationType (juce::Justification::centredRight);
+        applyLabelTextColour (*prefixLabel, scheme.text);
+        prefixLabel->setBorderSize (juce::BorderSize<int> (0));
+        prefixLabel->setFont (f);
+        aw->addAndMakeVisible (prefixLabel);
 
         suffixLabel = new juce::Label ("suffix", suffixText);
         suffixLabel->setComponentID (kPromptSuffixLabelId);
@@ -2647,7 +2657,7 @@ void FREQTRAudioProcessorEditor::openNumericEntryPopupForSlider (juce::Slider& s
 
         const int maxInputTextW = juce::jmax (1, stringWidth (f, worstCaseText));
 
-        layoutValueAndSuffix = [aw, te, suffixLabel, editorBaseBounds, isPercentPrompt, suffixText, suffixTextShort, maxInputTextW]()
+        layoutValueAndSuffix = [aw, te, prefixLabel, suffixLabel, editorBaseBounds, isPercentPrompt, prefixText, suffixText, suffixTextShort, maxInputTextW]()
         {
             juce::ignoreUnused (isPercentPrompt);
             const int contentPad = kPromptInlineContentPadPx;
@@ -2656,10 +2666,11 @@ void FREQTRAudioProcessorEditor::openNumericEntryPopupForSlider (juce::Slider& s
             const int availableW = contentRight - contentLeft;
             const int contentCenter = (contentLeft + contentRight) / 2;
 
+            const int prefixW = prefixText.isNotEmpty() ? stringWidth (prefixLabel->getFont(), prefixText) : 0;
             const int fullLabelW = stringWidth (suffixLabel->getFont(), suffixText) + 2;
             const bool stickPercentFull = suffixText.containsChar ('%');
             const int spaceWFull = stickPercentFull ? 0 : juce::jmax (2, stringWidth (suffixLabel->getFont(), " "));
-            const int worstCaseFullW = maxInputTextW + spaceWFull + fullLabelW;
+            const int worstCaseFullW = prefixW + maxInputTextW + spaceWFull + fullLabelW;
 
             const bool useShort = (worstCaseFullW > availableW) && suffixTextShort != suffixText;
             const juce::String& activeSuffix = useShort ? suffixTextShort : suffixText;
@@ -2681,14 +2692,14 @@ void FREQTRAudioProcessorEditor::openNumericEntryPopupForSlider (juce::Slider& s
                                               textW + (kEditorTextPadPx * 2));
             er.setWidth (editorW);
 
-            const int combinedW = textW + minGapPx + labelW;
+            const int combinedW = prefixW + textW + minGapPx + labelW;
 
             int blockLeft = contentCenter - (combinedW / 2);
             const int minBlockLeft = contentLeft;
             const int maxBlockLeft = juce::jmax (minBlockLeft, contentRight - combinedW);
             blockLeft = juce::jlimit (minBlockLeft, maxBlockLeft, blockLeft);
 
-            int teX = blockLeft - ((editorW - textW) / 2);
+            int teX = blockLeft + prefixW - ((editorW - textW) / 2);
             const int minTeX = contentLeft;
             const int maxTeX = juce::jmax (minTeX, contentRight - editorW);
             teX = juce::jlimit (minTeX, maxTeX, teX);
@@ -2697,6 +2708,9 @@ void FREQTRAudioProcessorEditor::openNumericEntryPopupForSlider (juce::Slider& s
             te->setBounds (er);
 
             const int textLeftActual = er.getX() + (er.getWidth() - textW) / 2;
+            if (prefixLabel != nullptr)
+                prefixLabel->setBounds (textLeftActual - prefixW, er.getY(), prefixW, juce::jmax (1, er.getHeight()));
+
             int labelX = textLeftActual + textW + minGapPx;
             const int minLabelX = contentLeft;
             const int maxLabelX = juce::jmax (minLabelX, contentRight - labelW);
@@ -2708,6 +2722,11 @@ void FREQTRAudioProcessorEditor::openNumericEntryPopupForSlider (juce::Slider& s
         };
 
         te->setBounds (editorBaseBounds);
+        if (prefixLabel != nullptr)
+        {
+            const int prefixW0 = prefixText.isNotEmpty() ? stringWidth (prefixLabel->getFont(), prefixText) : 0;
+            prefixLabel->setBounds (r.getX() - prefixW0, r.getY() + 1, prefixW0, juce::jmax (1, r.getHeight() - 2));
+        }
         int labelW0 = stringWidth (suffixLabel->getFont(), suffixText) + 2;
         suffixLabel->setBounds (r.getRight() + 2, r.getY() + 1, labelW0, juce::jmax (1, r.getHeight() - 2));
 
@@ -2851,7 +2870,11 @@ void FREQTRAudioProcessorEditor::openNumericEntryPopupForSlider (juce::Slider& s
     if (suffixLabel != nullptr && ! editorBaseBounds.isEmpty())
     {
         if (auto* te = aw->getTextEditor ("val"))
+        {
+            if (prefixLabel != nullptr)
+                prefixLabel->setFont (te->getFont());
             suffixLabel->setFont (te->getFont());
+        }
         if (layoutValueAndSuffix)
             layoutValueAndSuffix();
     }
@@ -2889,7 +2912,11 @@ void FREQTRAudioProcessorEditor::openNumericEntryPopupForSlider (juce::Slider& s
         if (auto* suffixLbl = dynamic_cast<juce::Label*> (aw->findChildWithID (kPromptSuffixLabelId)))
         {
             if (auto* te = aw->getTextEditor ("val"))
+            {
+                if (prefixLabel != nullptr)
+                    prefixLabel->setFont (te->getFont());
                 suffixLbl->setFont (te->getFont());
+            }
         }
 
         if (layoutValueAndSuffix)
